2e7cfff3336fbbc054d79a93e864557865c631f7
gustavoedgeti-afk <gustavo.edgeti@gmail.com>
2026-06-30 17:32:11 -0300

    criação CNAME

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,12 @@
 module.exports = function(eleventyConfig) {
   // Garante que o CSS, JS e imagens sejam movidos
+  eleventyConfig.addPassthroughCopy("CNAME"); 
   eleventyConfig.addPassthroughCopy("assets");
 
   return {
-    pathPrefix: "/site-focusinfraestrutura/",
+    pathPrefix: "/",
     dir: {
-      input: ".",        // Lê os arquivos HTML da raiz do repositório
+      input: "src",        // Lê os arquivos HTML da raiz do repositório
       output: "_site"    // Cospe o resultado na pasta _site
     }
   };
